f78c82ada092337769e0b229ef6528393d328be2
waterfall <cvb575859@gmail.com>
2024-04-21 00:57:57 +0900

    style: button 미세 조정

--- ui/button.tsx
+++ ui/button.tsx
@@ -8,7 +8,8 @@ export function Button({ children }: ButtonProps) {
   return (
     <button
       onClick={() => signIn('google', { callbackUrl: '/dashboard' })}
-      className="mt-6 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline  "
+      className="flex items-center justify-between px-4 py-2
+       bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md transition-colors duration-200 ease-in-out"
     >
       {children}
     </button>
