503317734ff64e5d15f6669463f3bb593b76c445
waterfall <cvb575859@gmail.com>
2024-04-30 21:26:35 +0900

    refactor: 주석 코드 제거
    사용하지 않는 코드 제거

--- components/email/email-list.tsx
+++ components/email/email-list.tsx
@@ -1,81 +1,26 @@
 import { Email } from '@/lib/definition';
 import { fetchEmails } from '@/lib/Emails';
 import { Button } from '@/ui/button';
-import { EmailItem } from './email-item';
-import { useEffect, useState } from 'react';
-import { DeleteTag, fetchTags } from '@/lib/Tag';
-import {
-  CheckCircleIcon,
-  EnvelopeOpenIcon,
-  PlusIcon,
-} from '@heroicons/react/20/solid';
+import { useState } from 'react';
+import { EnvelopeOpenIcon } from '@heroicons/react/20/solid';
 import { useRouter } from 'next/router';
-import Tags from '../tag/tag';
-import TagModal from '../tag/tag-modal';
 import ListDetail from './list-detail';
 
 export default function EmailList({ emails }: { emails: Email[] }) {
   const [email, setEmail] = useState<Email[]>(emails);
-  const [tags, setTags] = useState<string[]>([]);
-  const [selectedTags, setSelectedTags] = useState<Set<string>>(new Set());
 
-  // useEffect(() => {
-  //   const loadTags = async () => {
-  //     const fetchedTags = await fetchTags();
-  //     setTags(fetchedTags.map((tag: { name: string }) => tag.name));
-  //   };
-  //   loadTags();
-  // }, []);
+  const router = useRouter();
 
   const handleFetchEmails = async () => {
     try {
       const res = await fetchEmails();
       setEmail(res);
+      router.reload();
     } catch (error) {
       console.error('Error fetching emails:', error);
       alert('Error fetching emails');
     }
   };
-
-  // const handleDeleteTag = async (tag: string) => {
-  //   try {
-  //     const response = await DeleteTag(tag);
-  //     if (response) {
-  //       setTags(tags.filter((t) => t !== tag));
-  //       router.reload();
-  //     }
-  //   } catch (error) {
-  //     console.error('Failed to delete tag:', error);
-  //   }
-  // };
-
-  // const handleOpenModal = () => {
-  //   setIsModalOpen(true);
-  // };
-
-  // const onAddTag = (tag: string) => {
-  //   const updatedTags = [...tags, tag];
-  //   setTags(updatedTags);
-  // };
-
-  const toggleTagSelection = (tag: string) => {
-    const newSet = new Set(selectedTags);
-    if (newSet.has(tag)) {
-      newSet.delete(tag);
-    } else {
-      newSet.add(tag);
-    }
-    setSelectedTags(newSet);
-  };
-
-  // const filteredEmails = currentEmails.filter(
-  //   (email) =>
-  //     selectedTags.size === 0 ||
-  //     Array.from(selectedTags).some((tag) => email.subject.includes(tag))
-  // );
-
-  // // 페이지 번호를 설정하는 함수
-  // const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
     <main>
@@ -89,61 +34,6 @@ export default function EmailList({ emails }: { emails: Email[] }) {
           </Button>
         </div>
       </header>
-      {/* <article className="w-2/3 bg-white p-4 shadow-lg rounded-xl text-center flex flex-col items-center justify-center mx-auto mb-8 border">
-        <div className="w-full flex items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
-          <div className="w-full flex items-center justify-start px-4 py-2 sm:px-6 lg:px-8">
-            <Button
-              onClick={handleOpenModal}
-              className="bg-white hover:bg-blue-200 font-bold m-2 py-3 px-4 rounded-lg flex items-center border border-blue-200 "
-            >
-              <p className="text-gray-700 mr-1">태그 추가</p>
-              <PlusIcon className="h-5 w-5 text-blue-500" />
-            </Button>
-
-            <Tags
-              tags={tags}
-              selectedTags={selectedTags}
-              toggleTagSelection={toggleTagSelection}
-              onDeleteTag={handleDeleteTag}
-            />
-          </div>
-          <div className="flex items-center justify-end sm:px-6 lg:px-8">
-            <CheckCircleIcon
-              onClick={() => router.reload()}
-              className="h-8 w-8 text-green-500 cursor-pointer"
-            />
-          </div>
-        </div>
-
-        <div className="w-full">
-          {filteredEmails.map((email) => (
-            <EmailItem key={email.id} email={email} />
-          ))}
-        </div>
-        {isModalOpen && (
-          <TagModal onClose={() => setIsModalOpen(false)} onAddTag={onAddTag} />
-        )}
-        <div className="flex items-center justify-center gap-2 w-full p-4 sm:px-6 lg:px-8">
-          {Array.from(
-            { length: Math.ceil(emails.length / emailsPerPage) },
-            (_, i) => (
-              <Button
-                key={i + 1}
-                onClick={() => {
-                  paginate(i + 1);
-                }}
-                className={`${
-                  currentPage === i + 1
-                    ? 'bg-blue-500 text-white'
-                    : 'bg-white text-gray-800'
-                } border border-blue-200 hover:bg-blue-200 hover:border-blue-400 font-bold px-2 rounded-lg shadow-lg `}
-              >
-                {i + 1}
-              </Button>
-            )
-          )}
-        </div>
-      </article> */}
       <ListDetail email={email} />
     </main>
   );
--- components/email/list-detail.tsx
+++ components/email/list-detail.tsx
@@ -1,17 +1,14 @@
-import { fetchTags } from '@/lib/Tag';
 import { Email } from '@/lib/definition';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { EmailItem } from './email-item';
-import TagModal from '../tag/tag-modal';
-import { Button } from '@/ui/button';
 import { CheckCircleIcon } from '@heroicons/react/20/solid';
 import { useRouter } from 'next/router';
-import Pagenation from '../page/pagenation';
+import Pagenation from '../pagenation/pagenation';
+import Tag from '../tag/tag';
 
 export default function ListDetail({ email }: { email: Email[] }) {
   const router = useRouter();
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [emailsPerPage] = useState(20);
 
@@ -21,47 +18,10 @@ export default function ListDetail({ email }: { email: Email[] }) {
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
-  // useEffect(() => {
-  //   const loadTags = async () => {
-  //     const fetchedTags = await fetchTags();
-  //     setTags(fetchedTags.map((tag: { name: string }) => tag.name));
-  //   };
-  //   loadTags();
-  // }, []);
-
-  // const onAddTag = async (tag: string) => {};
-
-  // const filteredEmails = currentEmails.filter(async (email) => {
-  //   const tags = await loadTags();
-  //   return (
-  //     tags.length === 0 ||
-  //     tags.some((tag: string) => email.subject.includes(tag))
-  //   );
-  // });
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
   return (
     <main className="w-2/3 bg-white p-4 shadow-lg rounded-xl text-center flex flex-col items-center justify-center mx-auto mb-8 border">
       <header className="w-full flex items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
-        <div className="w-full flex items-center justify-start px-4 py-2 sm:px-6 lg:px-8">
-          <button
-            onClick={handleOpenModal}
-            className="bg-white hover:bg-blue-200 font-bold m-2 py-3 px-4 rounded-lg flex items-center border border-blue-200 "
-          >
-            <p className="text-gray-700 mr-1">태그 추가</p>
-            {/* <PlusIcon className="h-5 w-5 text-blue-500" /> */}
-          </button>
-
-          {/* <Tags
-            tags={tags}
-            selectedTags={selectedTags}
-            toggleTagSelection={toggleTagSelection}
-            onDeleteTag={handleDeleteTag}
-          /> */}
-        </div>
+        <Tag />
         <div className="flex items-center justify-end sm:px-6 lg:px-8">
           <CheckCircleIcon
             onClick={() => router.reload()}
@@ -71,11 +31,10 @@ export default function ListDetail({ email }: { email: Email[] }) {
       </header>
 
       <ul className="w-full">
-        {email.map((email) => (
+        {currentEmails.map((email) => (
           <EmailItem key={email.id} email={email} />
         ))}
       </ul>
-      {isModalOpen && <TagModal isModal={isModalOpen} />}
       <Pagenation
         email={email}
         emailsPerPage={emailsPerPage}
